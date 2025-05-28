b86b25f66ee6a792ac7f2555a28fda2615ff9f66
Naman2166 <142247109+Naman2166@users.noreply.github.com>
2025-05-29 00:00:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
         
         <div className="container mx-auto px-4 "> 
           <h2 className="text-3xl font-bold text-center mb-12">Our Menu Categories</h2>
-          <div className="grid grid-cols-2 px-2 gap-4 md:grid-cols-2 lg:grid-cols-3 sm:px-14 sm:px-10 sm:gap-10">
+          <div className="grid grid-cols-2 px-2 gap-4 md:grid-cols-2 lg:grid-cols-3 sm:px-14 sm:px-6 sm:gap-10">
             {categories.map((category, index) => (
               <FoodCategory 
                 key={index}
